96e11f03ddc55005e59c1d0f23e8df6a298f82bd
SavageCabbage39 <ty.chandrakasem@ascte.org>
2023-09-05 19:30:13 -0500

    Probably broke something LOL

--- FunGaem.py
+++ FunGaem.py
@@ -1,18 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 import random
 
 #app window
 window = tk.Tk()
 window.title("Roll the dice!")
 window.geometry("500x500")
+window.protocol("WM_DELETE_WINDOW", lambda:None) #Prevents closing the tab with X
+
+# Load and display the background image
+background_image = tk.PhotoImage(file="Assets/dice.jpg")
+
+# Create a label to display the background image
+background_label = tk.Label(window, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+# Send the background label to the back so other widgets can appear on top of it
+background_label.lower()
 
 #Start Screen
 fontstyle=("Comic Sans MS", 35)
-start_label = ttk.Label(master = window, text="click to roll",font=fontstyle)
+start_label = tk.Label(
+    master=window,
+    text="click to roll",
+    font=fontstyle,
+    anchor="n",
+    borderwidth=0
+    )
 start_label.pack()
 
-#dice
+
+
+
+#dice generator
 def roll():
     dice = random.randint(1,20)
     number.set(dice)
@@ -22,18 +43,15 @@ def roll():
 
 
 
-
 #input
-input_frame = ttk.Frame(master=window)
-button = ttk.Button(master = input_frame, text="roll", command=roll)
+button = ttk.Button(master = window, text="roll", command=roll)
 button.pack()
-input_frame.pack(pady=10)
+
 
 #output
 number=tk.StringVar()
-bottom_text = ttk.Label(master=window, text="Bottom text", font="Calibri 77", textvariable=number)
-bottom_text.pack(pady=5)
-
+bottom_text = tk.Label(master= window, text="Bottom text", font="Calibri 77", textvariable=number,anchor="center",borderwidth=0)
+bottom_text.pack(pady=100)
 
 
 #run
